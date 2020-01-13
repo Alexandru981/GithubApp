ef8ff203c9299c0b9a13e9247535e9f810f1629e
FWAlex <alex@futureworkshops.com>
2020-01-13 18:50:01 +0200

    Initial Commit

---
+++ AlexGithubTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlexGithubTestApp
+//
+//  Created by Alex Miculescu on 13/01/2020.
+//  Copyright © 2020 Alex Miculescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
